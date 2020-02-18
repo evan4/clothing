e6d21e1662a7a94464fff770cb6e24e903ffbd43
Ivan <evan4mc@gmail.com>
2020-02-18 15:34:24 +0500

    update shop data & cart utils

--- src/components/collection-item/CollectionItem.js
+++ src/components/collection-item/CollectionItem.js
@@ -1,7 +1,14 @@
 import React from 'react'
+import { connect } from 'react-redux';
+
 import "./collection-item.styles.scss";
 
-export default function CollectionItem({id, name, price, imageUrl}) {
+import CustomButton from '../custom-button/CustomButton';
+import { addItem } from '../../redux/cart/cart.actions';
+
+function CollectionItem({item, addItem}) {
+  const {name, price, imageUrl} = item;
+
   return (
     <div className="collection-item">
       <div className="image"
@@ -13,6 +20,13 @@ export default function CollectionItem({id, name, price, imageUrl}) {
         <span className="name">{name}</span>
         <span className="price">{price}</span>
       </div>
+      <CustomButton onClick={() => addItem(item)} inverted>Add to cart</CustomButton>
     </div>
   )
 }
+
+const mapDispatchToProps = dispatch => ({
+  addItem: item => dispatch(addItem(item))
+})
+
+export default connect(null, mapDispatchToProps)(CollectionItem);
--- src/components/custom-button/CustomButton.js
+++ src/components/custom-button/CustomButton.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import './custom-button.styles.scss';
 
-export default function CustomButton({children, isGoogleSingIn,  ...otherProps}) {
+export default function CustomButton(
+  {children, isGoogleSingIn, inverted,  ...otherProps}) {
   return (
-    <button className={`${isGoogleSingIn ? 'google-sing-in' : ''} custom-button`} {...otherProps}>
+    <button className={` ${inverted ? 'inverted' : ''}
+    ${isGoogleSingIn ? 'google-sing-in' : ''} custom-button `} {...otherProps}>
       {children}
     </button>
   )
